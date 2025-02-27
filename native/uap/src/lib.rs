e0935e076b480d77e1fd5d60ff33ba469cf9d581
Adam Rutkowski <hq@mtod.org>
2025-02-27 10:50:21 +0100

    !fixup

--- native/uap/src/lib.rs
+++ native/uap/src/lib.rs
@@ -1,9 +1,7 @@
 use once_cell::sync::Lazy;
 use rustler::NifResult;
 use std::sync::Mutex;
-use uaparser::{Parser, UserAgentParser}; // Import Parser trait explicitly
-
-// Initialize the parser once and store it globally
+use uaparser::{Parser, UserAgentParser};
 static PARSER: Lazy<Mutex<UserAgentParser>> = Lazy::new(|| {
     let parser = UserAgentParser::builder()
         .build_from_yaml("regexes.yaml")
@@ -14,9 +12,26 @@ static PARSER: Lazy<Mutex<UserAgentParser>> = Lazy::new(|| {
 rustler::init!("Elixir.UapNif", [parse_ua]);
 
 #[rustler::nif]
-fn parse_ua(user_agent: String) -> NifResult<String> {
-    let parser = PARSER.lock().unwrap(); // Lock the parser for thread safety
-    let result = parser.parse(&user_agent); // Now this should work
-    let family = result.user_agent.family;
-    Ok(family.to_string())
+fn parse_ua(
+    user_agent: String,
+) -> NifResult<(
+    String,
+    Option<String>,
+    Option<String>,
+    String,
+    Option<String>,
+    String,
+    Option<String>,
+)> {
+    let parser = PARSER.lock().unwrap();
+    let result = parser.parse(&user_agent);
+    Ok((
+        result.device.family.to_string(),
+        result.device.brand.map(|s| s.to_string()),
+        result.device.model.map(|s| s.to_string()),
+        result.os.family.to_string(),
+        result.os.major.map(|s| s.to_string()),
+        result.user_agent.family.to_string(),
+        result.user_agent.major.map(|s| s.to_string()),
+    ))
 }
